0a8b0e6e62c7f2de551ee8e68b3cd57ee26e3083
Guian Gumpac <guian.gumpac@gmail.com>
2026-05-03 08:27:08 -0700

    Fix for python 3.14

--- gremlin-python/src/main/python/tests/integration/driver/test_client.py
+++ gremlin-python/src/main/python/tests/integration/driver/test_client.py
@@ -155,8 +155,8 @@ async def async_connect(enable):
 
 
 def test_from_event_loop():
-    assert not asyncio.get_event_loop().run_until_complete(async_connect(False))
-    assert asyncio.get_event_loop().run_until_complete(async_connect(True))
+    assert not asyncio.run(async_connect(False))
+    assert asyncio.run(async_connect(True))
 
 
 def test_client_gremlin(client):
@@ -464,7 +464,7 @@ async def asyncio_func():
 
 def test_asyncio(client):
     try:
-        asyncio.get_event_loop().run_until_complete(asyncio_func())
+        asyncio.run(asyncio_func())
     except RuntimeError:
         assert False
 
